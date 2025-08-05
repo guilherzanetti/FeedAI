7100d90d960d0f9297f6055f02e346c8eb68f3ec
T2S | Guilherme Zanetti <zanettig520@gmail.com>
2025-08-05 16:52:29 -0300

    Ajustes

--- src/components/uiController.js
+++ src/components/uiController.js
@@ -2,10 +2,15 @@
 import { dom } from '../utils/domElements.js';
 
 export const uiController = {
+  //loading
   showLoading() { dom.loading.style.display = 'block'; },
   hideLoading() { dom.loading.style.display = 'none'; },
+
+  //popup
   showPopup() { dom.popupRender.style.display = 'flex'; },
   hidePopup() { dom.popupRender.style.display = 'none'; },
+
+  //canvas
   showCanvas() { dom.canvasDiv.style.display = 'block'; },
   hideCanvas() { dom.canvasDiv.style.display = 'none'; },
 };
--- src/utils/downloadHelper.js
+++ src/utils/downloadHelper.js
@@ -1,5 +1,22 @@
-// src/utils/downloadHelper.js
-// A biblioteca html2canvas precisa estar disponível globalmente ou ser importada
-export function saveDivAsPng(divId, filename = 'div-image.png') {
-  // Sua função saveDivAsPng original vai aqui
+export function saveDivAsPng(divId, filename = 'div-image.png', quality) {
+  const element = document.getElementById(divId);
+
+  if (!element) {
+    console.error('Div não encontrada.');
+    return;
+  }
+
+  html2canvas(element, { scale: quality})
+    .then((canvas) => {
+      const dataURL = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.href = dataURL;
+      link.download = filename;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    })
+    .catch((error) => {
+      console.error('Erro ao salvar a div como PNG:', error);
+    });
 }
